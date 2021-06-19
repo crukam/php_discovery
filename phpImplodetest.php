5b5b6ed7992287c0e3fb9f8ec4698902e696e1d3
crukam <jnkurunz0912@gmail.com>
2021-06-19 21:58:09 +0100

    more coding training

--- phpImplodetest.php
+++ phpImplodetest.php
@@ -21,3 +21,38 @@ function solutionRotationCycle($arrayInput, $rotationIndex) {
     return $arrayOutput;
 }
 
+function solutionTheOddOut($arrayInput) {
+ $countedItems = array_count_values($arrayInput);
+ $arrayFiltered = array_filter($countedItems,function($items) {return $items %2 == 1;});
+
+ foreach ($arrayFiltered as $key => $value) {
+     if($value % 2 != 0 ) return $key;
+ }
+ return null;
+}
+//$array_test = [];
+//solutionTheOddOut($array_test);
+
+function solMinMissingInteger($inputArray){
+    
+    $filteredArray = array_filter($inputArray, function($item) {
+        return $item > 0;
+    });
+
+    if(!$filteredArray) {
+        return 1;
+    }
+    
+    $arrayUniqueValues = array_unique($filteredArray);
+    
+    sort($arrayUniqueValues);
+
+    for ($i=1; $i <= COUNT($arrayUniqueValues) && $arrayUniqueValues[$i-1] == $i  ; $i++) 
+    ;
+    
+   return $i;
+}
+
+
+print_r(solutionTheOddOut([1, 1, 6, 4, 4,6,74]).PHP_EOL);
+//print_r(implode(',',range(1,1000)));
